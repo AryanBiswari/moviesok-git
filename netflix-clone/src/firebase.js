2a7ec0ec3803972b07186f7e7a2f1b5b066810e6
Aryan Biswari <aryanbiswari14@gmail.com>
2021-08-08 12:21:19 +0530

    three files deleted and one file added

---
+++ netflix-clone/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCvRNnXUdL0IAAOjKblWt_7LJw8qYiFI04",
+    authDomain: "moviesok-bb288.firebaseapp.com",
+    projectId: "moviesok-bb288",
+    storageBucket: "moviesok-bb288.appspot.com",
+    messagingSenderId: "418328918687",
+    appId: "1:418328918687:web:6cfe034828312193e0c0aa"
+  };
